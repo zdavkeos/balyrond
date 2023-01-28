aa7d3952a74ff70e6f71fa7ebfb660bc5c68bb42
Zach Davis <zdavkeos@gmail.com>
2023-01-27 20:52:34 -0600

    Cleanup and improve fw

--- firmware/main.py
+++ firmware/main.py
@@ -5,6 +5,7 @@
 
 import time
 import board
+import usb_cdc
 import rotaryio
 import supervisor
 from analogio import AnalogIn
@@ -17,19 +18,20 @@ enc = rotaryio.IncrementalEncoder(board.D9, board.D8)
 ain = AnalogIn(board.A2)
 
 while True:
-    # print("angle,distance, velocity")
 
     velocity = 0
-    enc.position = 0
+    enc.position = 0 # reset position each capture
     beg_t = time.monotonic()
     last_t = supervisor.ticks_ms()
     last_p = 0
-    while time.monotonic() < (beg_t + 10.0):
-        angle = (enc.position * deg_per_count) % 360.0
+
+    # wait for host program connect
+    while usb_cdc.console.connected:
+        angle = enc.position * deg_per_count % 360.0
         dist = ain.value * volts_per_count
 
         t = supervisor.ticks_ms()
-        if t > last_t:
+        if t > last_t and abs(angle - last_p) < 50:
             v = (angle - last_p) / (t - last_t)
             v = v * deg_per_ms_to_rpm
             velocity += (v - velocity) * 0.80
